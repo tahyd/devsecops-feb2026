b616e9cf26ccf07fe2551c6cd4a19055930ff10a
tahyd <ta.hyd.003@gmail.com>
2026-02-03 15:45:00 +0530

    pom modified

--- src/main/java/com/learn/ems/controllers/EmployeeController.java
+++ src/main/java/com/learn/ems/controllers/EmployeeController.java
@@ -1,15 +1,31 @@
 package com.learn.ems.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.learn.ems.entity.Employee;
+import com.learn.ems.services.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ems")
 public class EmployeeController {
+    @Autowired
+    private IEmployeeService employeeService;
     @GetMapping("/hello")
     public  String hello(){
         return "Hello World !";
     }
-
+    @PostMapping("/employee")
+    public Employee newEmployee(@RequestBody Employee employee){
+  return  employeeService.createEmployee(employee);
+    }
+  @GetMapping("/employee/{eid}")
+    public Employee getEmployee(@PathVariable("eid") int eid){
+        return employeeService.employeeById(eid);
+    }
+    @GetMapping("/employee")
+    public List<Employee> getEmployees(){
+        return  employeeService.employees();
+    }
 }
